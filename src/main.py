ee73e54636e0e4f78110e1f6968410222fb0089f
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2022-01-22 12:41:20 +0000

    Complete training pipeline

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import os
 
 from preprocessor import Preprocessor
 import keras_tuner as kt
+import tensorflow as tf
 from trainer import build_model
 
 
@@ -38,6 +39,43 @@ def main():
     df_x_test = get_x_test()
     preprocessor = Preprocessor(df_x_train, df_y_train, df_x_test)
     (training_data, test_data) = preprocessor.get_training_and_test_data()
+    MAX_EPOCHS = 20
+    count = 1
+    predictions = None
+    for target_id in training_data:
+        print(f"Training for asset {count}")
+        count+=1
+        (x, y) = training_data[target_id]
+        tuner = kt.Hyperband(
+            build_model,
+            objective="val_accuracy",
+            max_epochs=MAX_EPOCHS,
+            hyperband_iterations=10,
+        )
+        early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+        tuner.search(x, y, epochs=50, validation_split=0.2, callbacks=[early_stopping])
+        best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+        model = tuner.hypermodel.build(best_hps)
+        history = model.fit(x, y, epochs=50, validation_split=0.2)
+        val_acc_per_epoch = history.history['val_accuracy']
+        best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
+        hypermodel = tuner.hypermodel.build(best_hps)
+        hypermodel.fit(x, y, epochs=best_epoch, validation_split=0.2)
+        (ids, x_test) = test_data[target_id]
+        if predictions:
+            (prev_ids, preds) = predictions
+            predictions = (
+                np.append(prev_ids, ids),
+                np.append(preds, hypermodel.predict(x_test)),
+            )
+        else:
+            preds = hypermodel.predict(x_test)
+            (n, _) = preds.shape
+            predictions = (ids, preds.reshape((n,)))
+
+    predictions = np.column_stack(predictions)
+    df = produce_dataframe(predictions)
+    save_to_csv(df)
 
 
 if __name__ == "__main__":
